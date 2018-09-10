4514c560b4e71f2a7903b07d5565c9a44249bde8
eduardobarajas <barajas.eduardo@uabc.edu.mx>
2018-09-09 19:20:18 -0700

    Se agregaron unos campos en la clase como los arrays que contendran la informacion sobre el ritmo cardiaco.

--- Datos/Sesion.java
+++ Datos/Sesion.java
@@ -4,8 +4,9 @@ import com.barajasoft.higienedelsueo.Entidades.Paciente;
 
 public class Sesion {
     private Paciente paciente;
+    private Integer[] meds_cardiacos = new Integer []{0,0,0,0};
     private int tiene_preocupaciones = 0;
-    private String nivel_estres = "";
+    private String[] nivel_estres = new String[]{"","","","",""};
     private static Sesion instance = null;
     public static Sesion getInstance(){
         if(instance==null)
@@ -28,11 +29,19 @@ public class Sesion {
         this.tiene_preocupaciones = tiene_preocupaciones;
     }
 
-    public String getNivel_estres() {
+    public String[] getNivel_estres() {
         return nivel_estres;
     }
 
-    public void setNivel_estres(String nivel_estres) {
-        this.nivel_estres = nivel_estres;
+    public void setNivel_estres(int index,String nivel_estres) {
+        this.nivel_estres[index] = nivel_estres;
+    }
+
+    public Integer[] getMeds_cardiacos() {
+        return meds_cardiacos;
+    }
+
+    public void setMeds_cardiacos(int index, int ritmo) {
+        this.meds_cardiacos[index] = ritmo;
     }
 }
