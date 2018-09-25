8e27df2b92c9b384d52ded712c91f25fb890845a
eduardoBarajas <barajas.eduardo@uabc.edu.mx>
2018-09-25 08:19:20 -0700

    Se hicieron correciones en codigo para las mediciones del ritmo cardiaco.

--- Dialogos/DlgRitmoCardiaco.java
+++ Dialogos/DlgRitmoCardiaco.java
@@ -11,7 +11,9 @@ import android.hardware.SensorManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,43 +27,43 @@ import com.samsung.android.sdk.sensorextension.SsensorEventListener;
 import com.samsung.android.sdk.sensorextension.SsensorExtension;
 import com.samsung.android.sdk.sensorextension.SsensorManager;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 public class DlgRitmoCardiaco extends Dialog implements SensorEventListener{
 
     private SensorManager sensorManager;
     private Sensor heart;
     private float ritmo_cardiaco;
+    private List<Float> mediciones = new LinkedList<>();
+    private float ritmo_cardiaco_promedio;
+    private boolean medicion_activa = false;
 
-    private Button btn_Start = null;
     private Button btn_Stop = null;
     private TextView ritmo;
+    private ProgressBar progressBar;
 
     public DlgRitmoCardiaco(@NonNull Context context, DlgResult listener) {
         super(context);
         setContentView(R.layout.dlg_ritmo_cardiaco);
         ritmo = findViewById(R.id.txtCardiac);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(75);
         sensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
 
         heart = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
-        if(heart != null){
-            Toast.makeText(context,"Si tiene sensor de ritmo cardiaco!!",Toast.LENGTH_SHORT).show();
-        }else{
+        if(heart == null){
             Toast.makeText(context,"No tiene sensor de ritmo cardiaco!!",Toast.LENGTH_SHORT).show();
-        }
-
-        btn_Start = findViewById(R.id.startButton);
-        btn_Start.setOnClickListener(e->{
+        }else{
             sensorManager.registerListener(this,heart,SensorManager.SENSOR_DELAY_FASTEST);
-        });
-
-       btn_Stop = findViewById(R.id.stopBtn);
+        }
+       btn_Stop = findViewById(R.id.AceptarBtn);
        btn_Stop.setOnClickListener(e->{
-            listener.result("dlg_ritmo_cardiaco",String.valueOf(127));
+            listener.result("dlg_ritmo_cardiaco",String.valueOf((int)ritmo_cardiaco_promedio/mediciones.size()));
             dismiss();
        });
-
-
     }
 
     @Override
@@ -74,13 +76,33 @@ public class DlgRitmoCardiaco extends Dialog implements SensorEventListener{
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if(sensorEvent.sensor.getType() == Sensor.TYPE_HEART_RATE){
-            if(sensorEvent.values[0]==0){
-                Log.e("Estas muerto","Y a parte ella no te quiere :) ");
+            if(progressBar.getProgress()==75){
+                btn_Stop.setVisibility(View.VISIBLE);
+                Toast.makeText(getContext(),"Se completo la medicion",Toast.LENGTH_SHORT).show();
             }else{
-                ritmo_cardiaco = sensorEvent.values[0];
+                if(ritmo_cardiaco!=0){
+                    medicion_activa = true;
+                    mediciones.add(ritmo_cardiaco);
+                    progressBar.setProgress(mediciones.size());
+                }else{
+                    mediciones.clear();
+                    progressBar.setProgress(0);
+                    if(medicion_activa){
+                        Toast.makeText(getContext(),"Se interrumpio la medicion, por favor vuelve a colocar el dedo sobre sensor",Toast.LENGTH_SHORT).show();
+                        medicion_activa = false;
+                    }
+                }
             }
-            Log.e("Ritmo cardiaco",String.valueOf(sensorEvent.values[0]));
-            ritmo.setText(String.valueOf(sensorEvent.values[0]));
+            ritmo_cardiaco_promedio = 0;
+            ritmo_cardiaco = sensorEvent.values[0];
+            if(mediciones.size()==0){
+                ritmo.setText(String.valueOf(ritmo_cardiaco));
+            }else{
+                for(Float f : mediciones){
+                    ritmo_cardiaco_promedio += f;
+                }
+                ritmo.setText(String.valueOf(ritmo_cardiaco_promedio/mediciones.size()));
+            }
         }
     }
 
